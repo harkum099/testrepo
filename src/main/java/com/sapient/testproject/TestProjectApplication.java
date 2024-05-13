95c46200a9720cead38be58c68017d523f9d0e13
harkum099 <132202258+harkum099@users.noreply.github.com>
2024-05-13 22:46:19 +0530

    Update TestProjectApplication.java

--- src/main/java/com/sapient/testproject/TestProjectApplication.java
+++ src/main/java/com/sapient/testproject/TestProjectApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TestProjectApplication.class, args);
 	}
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
